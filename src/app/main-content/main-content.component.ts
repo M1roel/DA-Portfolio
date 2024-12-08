0a2686b75c45139cae2970a6d26ea818166d30ec
M1roel <ppfautsch@yahoo.de>
2024-12-08 14:37:03 +0100

    optimize folder structure

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,16 +2,26 @@ import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { WhyMeComponent } from './why-me/why-me.component';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { SaidAboutMeComponent } from './said-about-me/said-about-me.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, HeroSectionComponent, WhyMeComponent],
+  imports: [CommonModule, NavbarComponent, HeroSectionComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, SaidAboutMeComponent, ContactMeComponent],
   templateUrl: './main-content.component.html',
   template: `
   <section>
+    <div><app-navbar></app-navbar></div>
     <div><app-hero-section></app-hero-section></div>
     <div><app-why-me></app-why-me></div>
+    <div><app-my-skills></app-my-skills></div>
+    <div><app-my-projects></app-my-projects></div>
+    <div><app-said-about-me></app-said-about-me></div>    
+    <div><app-contact-me></app-contact-me></div>
   </section>
   `,
   styleUrl: './main-content.component.scss',
---
+++ src/app/main-content/said-about-me/said-about-me.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-said-about-me',
+  standalone: true,
+  imports: [],
+  templateUrl: './said-about-me.component.html',
+  styleUrl: './said-about-me.component.scss'
+})
+export class SaidAboutMeComponent {
+
+}
