641e8726be5e23e371db80e73918d51f751246db
M1roel <ppfautsch@yahoo.de>
2024-12-08 14:24:34 +0100

    import new components to main-content and create navbar component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { WhyMeComponent } from './why-me/why-me.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HeroSectionComponent, WhyMeComponent],
   templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  template: `
+  <section>
+    <div><app-hero-section></app-hero-section></div>
+    <div><app-why-me></app-why-me></div>
+  </section>
+  `,
+  styleUrl: './main-content.component.scss',
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
