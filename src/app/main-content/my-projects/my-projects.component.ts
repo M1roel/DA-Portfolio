be25facf6cd1767af481417ee717f4af8664d922
M1roel <ppfautsch@yahoo.de>
2024-12-11 10:34:13 +0000

    import projects with ngFor in the my-skills component

--- src/app/main-content/my-projects/my-projects.component.ts
+++ src/app/main-content/my-projects/my-projects.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
-
+  projects = [
+    {
+      name: 'JOIN',
+      stack: 'HTML, CSS, JavaScript',
+      description: 'A description of Project 1.',
+      image: '../../../assets/img/join-project.png',
+      github: 'https://github.com/project1',
+      liveDemo: 'https://live-demo1.com'
+    },
+    {
+      name: 'Sharkie',
+      stack: 'HTML, CSS, JavaScript',
+      description: 'A description of Project 2.',
+      image: '../../../assets/img/sharkie-project.png',
+      github: 'https://github.com/project2',
+      liveDemo: 'https://live-demo2.com'
+    },
+    {
+      name: 'DaBubble',
+      stack: 'Vue.js, Firebase',
+      description: 'A description of Project 3.',
+      image: '../../../assets/img/join-project.png',
+      github: 'https://github.com/project3',
+      liveDemo: 'https://live-demo3.com'
+    }
+  ];
 }
