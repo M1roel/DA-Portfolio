c50609f6af1138e70eef08f93167e3f25ca202a5
M1roel <ppfautsch@yahoo.de>
2024-12-13 19:36:05 +0100

    start to optimize and style the siad-about-me component

--- src/app/main-content/said-about-me/said-about-me.component.ts
+++ src/app/main-content/said-about-me/said-about-me.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-said-about-me',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './said-about-me.component.html',
   styleUrl: './said-about-me.component.scss'
 })
 export class SaidAboutMeComponent {
-
+  abouts = [
+    {
+      name: 'Max Muster',
+      project: 'JOIN',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam'
+    },
+    {
+      name: 'Max Muster',
+      project: 'JOIN',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam'
+    },
+    {
+      name: 'Max Muster',
+      project: 'JOIN',
+      description: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam'
+    }
+  ];
 }
