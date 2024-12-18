f52f55b3071326dcec775add453a1ad01e53c956
M1roel <ppfautsch@yahoo.de>
2024-12-18 10:05:38 +0100

    optimized styling for my-projects component

--- src/app/main-content/my-projects/my-projects.component.ts
+++ src/app/main-content/my-projects/my-projects.component.ts
@@ -12,24 +12,24 @@ export class MyProjectsComponent {
   projects = [
     {
       name: 'JOIN',
-      stack: 'HTML, CSS, JavaScript',
-      description: 'A description of Project 1.',
+      stack: 'HTML | CSS | JavaScript | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
       image: '../../../assets/img/join-project.png',
-      github: 'https://github.com/project1',
+      github: 'https://github.com/M1roel/DA-Join',
       liveDemo: 'https://live-demo1.com'
     },
     {
       name: 'Sharkie',
-      stack: 'HTML, CSS, JavaScript',
-      description: 'A description of Project 2.',
+      stack: 'HTML | CSS | JavaScript',
+      description: 'Jump, run and throw game based on object-oriented approach. Help Sharkie to find coins and poisoned bottles to fight against the crazy orca.',
       image: '../../../assets/img/sharkie-project.png',
-      github: 'https://github.com/project2',
+      github: 'https://github.com/M1roel/DA-Sharkie',
       liveDemo: 'https://live-demo2.com'
     },
     {
-      name: 'DaBubble',
-      stack: 'Vue.js, Firebase',
-      description: 'A description of Project 3.',
+      name: 'CRM',
+      stack: 'Angular | TypeScript | SCSS | Firebase',
+      description: 'A CRM (Customer Relationship Management) system manages customer interactions, streamlines sales, and enhances customer service through data organization and communication tracking.',
       image: '../../../assets/img/join-project.png',
       github: 'https://github.com/project3',
       liveDemo: 'https://live-demo3.com'
