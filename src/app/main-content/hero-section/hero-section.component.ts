2d3060b72f469f9f001d5f4b1fbc7daa449bda6d
M1roel <ppfautsch@yahoo.de>
2024-12-31 15:06:08 +0100

    Enhance hero section layout with new arrow navigation and adjust styles for improved responsiveness

--- src/app/main-content/hero-section/hero-section.component.ts
+++ src/app/main-content/hero-section/hero-section.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './hero-section.component.scss'
 })
 export class HeroSectionComponent {
-
+  scrollToSection(event: Event, sectionId: string) {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      const yOffset = -100; // Adjust this value to change the scroll offset
+      const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+  }
 }
