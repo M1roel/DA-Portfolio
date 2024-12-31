68a5dbef67e1580f34bab4bd325a524b96947ab9
M1roel <ppfautsch@yahoo.de>
2024-12-31 16:44:51 +0100

    Remove unused translation imports and HttpLoaderFactory function from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
-import { HttpClientModule, HttpClient } from '@angular/common/http';
-import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 @Component({
   selector: 'app-root',
