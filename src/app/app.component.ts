4e04c62c5bffb979c44e7607a7235d06e3f443f9
M1roel <ppfautsch@yahoo.de>
2025-01-02 19:07:54 +0100

    Add ngx-translate for internationalization and update navbar for language support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { NavbarComponent } from "./shared/components/navbar/navbar.component";
+import { TranslateModule } from "@ngx-translate/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, NavbarComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'DA-Portfolio';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    const browserLang = this.translate.getBrowserLang();
+    if (browserLang && browserLang.match(/en|de/)) {
+      this.translate.use(browserLang);
+    } else {
+      this.translate.use('en');
+    }
+  }
 }
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,17 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   menuOpen: boolean = false;
 
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
+  }
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  useLanguage(event: Event, language: string): void {
+    event.preventDefault(); // Prevent the default link behavior
+    this.translate.use(language);
   }
 }
