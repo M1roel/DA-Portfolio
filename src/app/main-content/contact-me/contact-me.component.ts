c88529cde8c6be39558a07757fee91958715ed76
M1roel <ppfautsch@yahoo.de>
2025-01-08 20:37:48 +0100

    Improve contact form validation messages and placeholders for better user guidance

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -62,17 +62,11 @@ export class ContactMeComponent {
    */
   async checkName(): Promise<boolean> {
     const namePattern = /^[A-Za-z]{3,}$/;
-    const nameInput = document.getElementById('name') as HTMLInputElement;
-
     if (!namePattern.test(this.contactData.name)) {
-      this.nameError = 'Your name is required';
-      nameInput.classList.add('input-error');
-      nameInput.classList.remove('input-done');
+      this.nameError = 'Name is required';
       return false;
     } else {
       this.nameError = '';
-      nameInput.classList.remove('input-error');
-      nameInput.classList.add('input-done');
       return true;
     }
   }
@@ -84,14 +78,11 @@ export class ContactMeComponent {
    */
   async checkEmail(): Promise<boolean> {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    const emailInput = document.getElementById('email') as HTMLInputElement;
     if (!emailPattern.test(this.contactData.email)) {
-      this.emailError = 'Your Email is required';
-      emailInput.classList.add('input-error');
+      this.emailError = 'Valid email is required';
       return false;
     } else {
       this.emailError = '';
-      emailInput.classList.remove('input-error');
       return true;
     }
   }
@@ -103,13 +94,11 @@ export class ContactMeComponent {
    */
   async checkMessage(): Promise<boolean> {
     const wordCount = this.contactData.message.trim().split(/\s+/).length;
-    const messageInput = document.getElementById('message') as HTMLInputElement;
     if (wordCount < 4) {
-      this.messageError = 'Your message is required';
+      this.messageError = 'Message must be at least 4 words';
       return false;
     } else {
       this.messageError = '';
-      messageInput.classList.remove('input-error');
       return true;
     }
   }
